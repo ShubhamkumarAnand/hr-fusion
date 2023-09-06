c4a9aa1a51093f47175cd1d2e25a973d6f57ab25
imskanand <the.anand3099@gmail.com>
2023-09-06 14:27:56 +0530

    making it for the desktop browser & added images

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,37 +11,41 @@ const page = () => {
           </Link>
           <Image src="img/hamburger.svg" alt="hamburger-menu" height={25} width={20} />
         </nav>
-        <main className="mt-36 h-full flex-col">
-          <div className="my-10 space-y-2">
-            <h2 className="text-3xl font-semibold tracking-wide">Welcome back</h2>
-            <p className="text-sm text-gray-700">Welcome back! Please enter your details.</p>
+        <main className="mt-24 h-full lg:flex">
+          <div className="md:w-1/2 lg:w-1/3 lg:mr-20 max-w-md">
+            <div className="my-10 space-y-2">
+              <h2 className="text-3xl font-semibold">Welcome back 🤗</h2>
+              <p className="text-sm text-gray-500">Welcome back! Please enter your details.</p>
+            </div>
+              <input
+                type="email"
+                required
+                minLength={5}
+                placeholder="Email"
+                className="w-full border-b-[1px] border-gray-500 py-3 text-lg outline-none"
+              />
+              <input
+                type="password"
+                required
+                minLength={5}
+                placeholder="Password"
+                className="mb-7 mt-5 w-full border-b-[1px] border-gray-500 py-3 text-lg text-gray-700 outline-none"
+              />
+              <button className="w-full rounded-md bg-black p-3 font-semibold text-white">Sign in</button>
+              <button className="my-5 w-full rounded-md border-[1px] border-gray-700 bg-white p-3 font-semibold">
+                Sign in with Google
+              </button>
+
+            <div className="flex items-center justify-center text-sm">
+              <p>Need an account ? &nbsp;</p>
+              <Link href="/signup" className="border-spacing-0 border-b-[1px] border-black font-semibold">
+                {" "}
+                Create an account
+              </Link>
+            </div>
           </div>
-          <form action="">
-            <input
-              type="email"
-              required
-              minLength={5}
-              placeholder="Email"
-              className="w-full border-b-[1px] border-gray-500 py-3 text-lg outline-none"
-            />
-            <input
-              type="password"
-              required
-              minLength={5}
-              placeholder="Password"
-              className="mb-7 mt-5 w-full border-b-[1px] border-gray-500 py-3 text-lg text-gray-700 outline-none"
-            />
-            <button className="w-full rounded-md bg-black p-3 font-semibold text-white">Sign in</button>
-            <button className="my-5 w-full rounded-md border-[1px] border-gray-700 bg-white p-3 font-semibold">
-              Sign in with Google
-            </button>
-          </form>
-          <div className="flex items-center justify-center text-sm">
-            <p>Need an account ? &nbsp;</p>
-            <Link href="/signup" className="border-spacing-0 border-b-[1px] border-black font-semibold">
-              {" "}
-              Create an account
-            </Link>
+          <div className="hidden lg:flex ">
+            <Image src="/img/hr-fusion.jpg" width={900} height={900} alt="hero-img" className="h-full"/>
           </div>
         </main>
       </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,44 +11,49 @@ const page = () => {
           </Link>
           <Image src="img/hamburger.svg" alt="hamburger-menu" height={25} width={20} />
         </nav>
-        <main className="mt-36 h-full flex-col">
-          <div className="my-10 space-y-2">
-            <h2 className="text-3xl font-semibold tracking-wide">Welcome</h2>
-            <p className="text-sm text-gray-700">Welcome to HR-Fusion! Please enter your details.</p>
+        <main className="mt-8 lg:mt-20 h-full lg:flex">
+          <div className="md:w-1/2 lg:w-1/3 lg:mr-20 max-w-md">
+            <div className="my-10 space-y-2">
+              <h2 className="text-3xl font-semibold tracking-wide">Welcome 💎</h2>
+              <p className="text-sm text-gray-700">Welcome to HR-Fusion! Please enter your details.</p>
+            </div>
+            <form action="">
+              <input
+                type="name"
+                required
+                minLength={6}
+                placeholder="Username"
+                className="w-full border-b-[1px] border-gray-500 py-3 text-lg outline-none"
+              />
+              <input
+                type="email"
+                required
+                minLength={5}
+                placeholder="Email"
+                className="my-5 w-full border-b-[1px] border-gray-500 py-3 text-lg outline-none"
+              />
+              <input
+                type="password"
+                required
+                minLength={8}
+                placeholder="Password"
+                className="mb-7 w-full border-b-[1px] border-gray-500 py-3 text-lg text-gray-700 outline-none"
+              />
+              <button className="w-full rounded-md bg-black p-3 font-semibold text-white">Sign up</button>
+              <button className="my-5 w-full rounded-md border-[1px] border-gray-700 bg-white p-3 font-semibold">
+                Sign up with Google
+              </button>
+            </form>
+            <div className="flex items-center justify-center text-sm">
+              <p>Already have an account ? &nbsp;</p>
+              <Link href="/login" className="border-spacing-0 border-b-[1px] border-black font-semibold">
+                {" "}
+                Log in
+              </Link>
+            </div>
           </div>
-          <form action="">
-            <input
-              type="name"
-              required
-              minLength={6}
-              placeholder="Username"
-              className="w-full border-b-[1px] border-gray-500 py-3 text-lg outline-none"
-            />
-            <input
-              type="email"
-              required
-              minLength={5}
-              placeholder="Email"
-              className="w-full my-5 border-b-[1px] border-gray-500 py-3 text-lg outline-none"
-            />
-            <input
-              type="password"
-              required
-              minLength={8}
-              placeholder="Password"
-              className="mb-7 w-full border-b-[1px] border-gray-500 py-3 text-lg text-gray-700 outline-none"
-            />
-            <button className="w-full rounded-md bg-black p-3 font-semibold text-white">Sign up</button>
-            <button className="my-5 w-full rounded-md border-[1px] border-gray-700 bg-white p-3 font-semibold">
-              Sign up with Google
-            </button>
-          </form>
-          <div className="flex items-center justify-center text-sm">
-            <p>Already have an account ? &nbsp;</p>
-            <Link href="/login" className="border-spacing-0 border-b-[1px] border-black font-semibold">
-              {" "}
-              Log in
-            </Link>
+          <div className="hidden lg:flex mt-10">
+            <Image src="/img/hr-fusion.jpg" width={900} height={900} alt="hr-fusion-auth"/>
           </div>
         </main>
       </div>
